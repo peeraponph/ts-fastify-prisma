236c363af72619efd6e14a4ceb4fd8e302d00f9d
peerapon.ph <peerapon.ph@kkumail.com>
2025-06-19 17:37:11 +0700

    fix: Update Kafka configuration in docker-compose for kafka UI

--- services/notification-service/src/consumer.ts
+++ services/notification-service/src/consumer.ts
@@ -1,14 +1,6 @@
-// import { consumer } from './kafka'
+import { consumer } from './kafka'
 import { handleUserCreated } from './handler/user-created.handler'
 
-import { Kafka } from 'kafkajs'
-
-const kafka = new Kafka({
-    clientId: 'notification-service',
-    brokers: ['host.docker.internal:9092'],  // หรือใช้ host.docker.internal ถ้ารันใน container
-})
-
-const consumer = kafka.consumer({ groupId: 'notification-group' })
 
 const start = async () => {
     await consumer.connect()
