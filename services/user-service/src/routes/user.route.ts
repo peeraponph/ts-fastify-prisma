c01098a3143b43e2c63fe78fa30a5271c057a8db
peerapon.ph <peerapon.ph@kkumail.com>
2025-06-20 16:04:10 +0700

    Refactor: Reintroduce Prisma schema and seed files, update .gitignore for generated files

--- services/user-service/src/routes/user.route.ts
+++ services/user-service/src/routes/user.route.ts
@@ -2,6 +2,7 @@ import { FastifyPluginAsync } from 'fastify'
 import { createUser, getAllUsers } from '../services/user.service'
 
 const userRoutes: FastifyPluginAsync = async (fastify) => {
+    
     fastify.post('/', async (request, reply) => {
         const { name, email } = request.body as { name: string; email: string }
         const user = await createUser(name, email)
